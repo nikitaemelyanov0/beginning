903f429e2cf1bd0d929e6291da03694c0d22fb95
Nikita Emelyanov <nik.emelyanov.1988@gmail.com>
2024-11-23 15:34:39 +0500

    aaaaa

---
+++ bd.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername ="151.248.115.10";
+$username = "root";
+$password = "Kwuy1mSu4Y";
+$dbname = "is364-Emelyanov";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'bd.php';
+
+$user = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+echo $email;
