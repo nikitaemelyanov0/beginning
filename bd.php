4e2808085d3d561258ddf0ff80cbca993357b92f
Nikita Emelyanov <nik.emelyanov.1988@gmail.com>
2024-11-23 16:11:45 +0500

    e

--- bd.php
+++ bd.php
@@ -4,5 +4,6 @@ $servername ="151.248.115.10";
 $username = "root";
 $password = "Kwuy1mSu4Y";
 $dbname = "is364-Emelyanov";
+$port = "3306";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
